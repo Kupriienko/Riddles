c3ae247f46b2cf415400a9334061db7efe04f9ff
Kupriienko <Kupriienko>
2022-08-28 13:03:20 +0300

    update

--- main.py
+++ main.py
@@ -7,61 +7,27 @@ con = sqlite3.connect('r_database.sqlite', check_same_thread=False)
 cur = con.cursor()
 
 
-def riddles_amount() -> int:
-    return cur.execute("select id from riddles order by id desc LIMIT 1;").fetchone()[0]
-
-
-def get_riddle(i) -> str:
-    return cur.execute("select riddle from riddles where id = ?", (i,)).fetchall()[0][0]
-
-
-def block_in_str(i: int) -> str:
-    return f'''            <div class="block">
-            <div class="riddle">{get_riddle(i)}</div>
-            <div class="answer">
-                <input type="text" name="answer"" >
-                <button>Перевірити</button>
-            </div>
-        </div>\n'''
-
-
-def initial_content(r_amount: int) -> None:
-    with open("index.html", "r") as f:
-        contents = f.readlines()
-    html_code = ""
-    for i in range(1, r_amount + 1):
-        html_code += block_in_str(i)
-    contents.insert(18, html_code)
-    with open("index.html", "w") as f:
-        contents = "".join(contents)
-        f.write(contents)
-
-
 @app.route('/addRiddle', methods=['POST'])
-def add_riddle() -> Response:
+def add_riddle() -> list[str, str]:
     data = request.get_json()
     riddle = [data["riddle"], data["answer"]]
     cur.execute("insert into riddles (riddle, solution) values (?, ?)", riddle)
     con.commit()
-    return jsonify(
-        riddle=riddle,
-    )
+    return riddle
 
 
 @app.route('/getAnswer', methods=['GET'])
-def get_answer() -> dict[str, int]:
+def get_answer() -> dict[str, bool]:
     data = request.get_json()
-    answer = cur.execute("select id, solution from riddles")
-    for i in answer.fetchall():
-        if i[0] == data["id"]:
-            if i[1] == data["answer"]:
-                return {"correct": 1}
-            else:
-                return {"correct": 2}
+    answer = cur.execute("select solution from riddles where id = ?", (data["id"],)).fetchone()[0]
+    return {"correct": answer == data["answer"]}
 
 
 @app.route('/')
 def index() -> str:
-    initial_content(riddles_amount())
-    with open('index.html', 'r') as contents:
-        return contents.read()
+    template_contents = open("index.html", "r").read()
+    riddle_contents = open("riddle.html", "r").read()
+    riddles = ""
+    for i in cur.execute("select riddle from riddles").fetchall():
+        riddles += riddle_contents.replace("(riddle)", i[0]) + "\n"
+    return "".join(template_contents.replace("(riddles)", riddles))
