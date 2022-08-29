1fcfab5a46d5b26ee7f6dc15153969434415e7c1
Kupriienko <Kupriienko>
2022-08-29 21:24:23 +0300

    single quotes correction

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ cur = con.cursor()
 @app.route('/addRiddle', methods=['POST'])
 def add_riddle() -> tuple[str, str]:
     data = request.get_json()
-    riddle = data["riddle"], data["answer"]
-    cur.execute("insert into riddles (riddle, solution) values (?, ?)", riddle)
+    riddle = data['riddle'], data['answer']
+    cur.execute('insert into riddles (riddle, solution) values (?, ?)', riddle)
     con.commit()
     return riddle
 
@@ -19,19 +19,18 @@ def add_riddle() -> tuple[str, str]:
 @app.route('/getAnswer', methods=['GET'])
 def get_answer() -> dict[str, bool]:
     data = request.get_json()
-    answer = cur.execute("select solution from riddles where id = ?", (data["id"],)).fetchall()
+    answer = cur.execute('select solution from riddles where id = ?', (data['id'],)).fetchall()
     if len(answer) == 1:
-        return {"correct": answer[0][0] == data["answer"]}
+        return {'correct': answer[0][0] == data['answer']}
     else:
         abort(404)
 
 
 @app.route('/')
 def index() -> str:
-    template_contents = open("index.html", "r").read()
-    riddle_contents = open("riddle.html", "r").read()
-    riddles = ""
-    for i in cur.execute("select riddle from riddles").fetchall():
-        riddles += riddle_contents.replace("{riddle}", i[0]) + "\n"
-    return "".join(template_contents.replace("{riddles}", riddles))
-
+    template_contents = open('index.html', 'r').read()
+    riddle_contents = open('riddle.html', 'r').read()
+    riddles = ''
+    for i in cur.execute('select riddle from riddles').fetchall():
+        riddles += riddle_contents.replace('{riddle}', i[0]) + '\n'
+    return ''.join(template_contents.replace('{riddles}', riddles))
