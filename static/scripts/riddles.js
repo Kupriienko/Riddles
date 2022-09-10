62b0fe62954b2b39cff5c9a3a7d86e4c6375dd2e
Kupriienko <Kupriienko>
2022-09-10 15:29:48 +0300

    add LocalStorage

--- static/scripts/riddles.js
+++ static/scripts/riddles.js
@@ -1,4 +1,12 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', async (event) => {
+    for (let id = 1; id < document.getElementsByTagName('button').length; id++) {
+        const userAnswer = JSON.parse(localStorage.getItem(`userAnswer-${id}`))
+        if (userAnswer !== null) {
+            const answer = document.getElementById(`input-${id}`);
+            answer.value = userAnswer[1];
+            answer.classList.add(`answer-${userAnswer[2]}`);
+        }
+    }
     for (const button of document.getElementsByTagName('button')) {
         button.addEventListener('click', () => verify(button.id));
     }
@@ -17,4 +25,5 @@ async function verify(id) {
     });
     const response = await initialResponse.json();
     answer.classList.add(`answer-${response['correct']}`)
+    localStorage.setItem(`userAnswer-${id}`, JSON.stringify([id, answer.value, response['correct']]))
 }
