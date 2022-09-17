360bb96a67a40fb9d1db2c97daa91e83f0f140c1
Kupriienko <Kupriienko>
2022-09-17 22:49:46 +0300

    moving from Sqlite to Postgresql

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify, request, Response, abort
-import sqlite3
+import psycopg2
 
 app = Flask(__name__)
 
-con = sqlite3.connect('r_database.sqlite', check_same_thread=False)
+con = psycopg2.connect(
+    host='localhost',
+    database='riddles.db',
+    user="postgres",
+    password="microsoft123",
+    port=5432
+)
 cur = con.cursor()
 
 
@@ -13,9 +19,9 @@ def add_riddle() -> str:
     if type(data) != dict or data.keys() != {'riddle', 'answer'}:
         abort(400)
     riddle = data['riddle'], data['answer']
-    cur.execute('insert into riddles (riddle, solution) values (?, ?)', riddle)
+    cur.execute('insert into riddles (riddle, solution) values (%s, %s) returning id', riddle)
     con.commit()
-    return str(cur.execute('select id from riddles where riddle = ?', (data['riddle'],)).fetchall()[0][0])
+    return str(cur.fetchone()[0])
 
 
 @app.route('/verifyAnswer', methods=['GET'])
@@ -23,7 +29,8 @@ def get_answer() -> dict[str, bool]:
     data = request.args.to_dict()
     if type(data) != dict or (data.keys() != {'answer', 'id'} or not data['id'].isnumeric()):
         abort(400)
-    answer = cur.execute('select solution from riddles where id = ?', (data['id'],)).fetchall()
+    cur.execute('select solution from riddles where id = %s', (data['id'],))
+    answer = cur.fetchall()
     if len(answer) == 1:
         return {'correct': answer[0][0] == data['answer']}
     else:
@@ -35,6 +42,7 @@ def index() -> str:
     index_template = open('index.html', 'r', encoding='utf-8').read()
     riddle_template = open('riddle.html', 'r', encoding='utf-8').read()
     riddles = ''
-    for i in cur.execute('select id, riddle from riddles').fetchall():
+    cur.execute('select id, riddle from riddles')
+    for i in cur.fetchall():
         riddles += riddle_template.replace('{id}', str(i[0])).replace('{riddle}', i[1]) + '\n'
     return ''.join(index_template.replace('{riddles}', riddles))
